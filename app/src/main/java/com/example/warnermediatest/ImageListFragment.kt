4b9583b1b3acc0d7f7f21085f312278632ca3536
David <david.plotzke@gmail.com>
2021-11-03 15:14:23 -0700

    Forgot to add newly created files for the previous commit.

---
+++ app/src/main/java/com/example/warnermediatest/ImageListFragment.kt
@@ -0,0 +1,30 @@
+package com.example.warnermediatest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class ImageListFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.image_list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button: Button = view.findViewById(R.id.button)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_imageListFragment_to_viewImageFragment, null)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/warnermediatest/ViewImageFragment.java
@@ -0,0 +1,22 @@
+package com.example.warnermediatest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ViewImageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.view_image_fragment, container, false);
+    }
+}
